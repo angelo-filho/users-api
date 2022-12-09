d3fcdfdee510e1c28a7c05e0c7bd31061ab38bc0
Angelo Filho <angelo.filho06@hotmail.com>
2022-12-09 15:46:48 -0300

    feat: Add turn user admin

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const userAlreadyExist = this.usersRepository.findById(user_id);
+
+    if (!userAlreadyExist) {
+      throw new Error("Mensagem de erro");
+    }
+
+    const user = this.usersRepository.turnAdmin(userAlreadyExist);
+
+    return user;
   }
 }
 
